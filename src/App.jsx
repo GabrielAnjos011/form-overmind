30fa6edbfd8e4d6907a3b674eaf1fbb1d16a7a2b
Gabriel Garcia dos Anjos <gabrielgarcia.anjos250@gmail.com>
2022-08-05 00:13:40 -0300

    feat: add react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { ToastContainer } from "react-toastify";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
-    <div>
-      <Home />
-
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
       <ToastContainer />
-    </div>
+    </BrowserRouter>
   );
 }
 
